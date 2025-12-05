aca9beaddec3a4281c0a8e8bd5ca8dbde3f0ba38
khaprepoonam <khaprepoonam54@gmail.com>
2025-12-05 19:46:20 +0530

    string

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -28,3 +28,35 @@ let someNumber = 99
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// *************************operations*************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Poonam"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("3" + 2); // "32"
+console.log(3 + "2"); // "32"
+console.log("1" + 2 + 3); // "123"
+console.log(1 + 2 + "3"); // "33"       
+console.log("4" - 2); // 2
+console.log("4" * 2); // 8      
+console.log("4" / 2); // 2
+console.log (("3" + 2)* 4 % 5 + 2); // 4
+
+console.log(+true); // 1
+console.log(+""); // 0  
+
+let num1, num2 , num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);  // gameCounter = gameCounter + 1
