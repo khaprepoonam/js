6259db8c90c36b2eedaa2ce9c3e6473ab4e46c16
khaprepoonam <khaprepoonam54@gmail.com>
2025-12-05 18:51:45 +0530

    add initial variable and data type

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId  = 1445534;
+let accountEmail = "poonam@gmail.com"
+var accountPassword = "12345";
+accountCity = "Mumbai";
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "67890"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3 + 2); // we are using nodejs, not browser
+
+console.log(3 + 2)
+console.log("Poonam")
+
+let name = "Poonam";
+let age = 19
+let isloggedIn = false
+let state; // undefined
+
+
+// number => 2 to power 53
+// bigint 
+// string
+// boolean => true/false
+// null 
+// undefined    
+// symbol
+
+
+
+
+// object
+
+console.log(typeof "Poonam")
+console.log(typeof age);
