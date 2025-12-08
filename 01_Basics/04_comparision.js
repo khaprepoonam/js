0ea401184f0f1b4ac9ef8465ab77e5daecac82b6
khaprepoonam <khaprepoonam54@gmail.com>
2025-12-08 16:19:37 +0530

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2 > 1);
+// console.log("2"> 1);
+
+console.log(null > 0); // false
+console.log(null >= 0); // true
+console.log(null == 0); // false
+console.log(undefined == 0); // false
+console.log(0 ==0 ); // true
+
+// ==== strict equality operator
+console.log("0" == 0); // true
+console.log("2"===2); // true
